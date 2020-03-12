3280ce5fbf735df9e3a2e9b7479aff3ea610f5ba
kopeckylukas <56082570+kopeckylukas@users.noreply.github.com>
2020-03-12 14:06:36 +0000

    Third Try

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import java.io.*;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+/**
 /**
  *
  * @author lukaskopecky
  */
 public class Main {
     
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
        Main exe = new Main();
        exe.execute();
     }
     
-    public void execute(){
-        DatabaseConnectivity connect = new DatabaseConnectivity();
-        connect.hello();
+    public void execute() throws Exception{
+        //DatabaseConnectivity connect = new DatabaseConnectivity();
+        DatabaseConnectivity.ReadSQLi();
         
     }
 }
